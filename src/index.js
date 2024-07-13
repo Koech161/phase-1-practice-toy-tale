cccc89d94a416181ec84c07fdbea305514e6d0ef
Abel Koech <koechabel161@gmail.com>
2024-07-13 18:51:17 +0300

    toy

--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ let addToy = false;
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+//const toyUrl=('http://localhost:3000/toys')
+  fetch('http://localhost:3000/toys')
+  .then(res=>res.json())
+  .then((completedata))
+  .catch(error=>console(error))
+  let data1=''
+ completedata.map((values)=>{
+    data1+=`<div class="card">
+      <h2>${values.name}</h2>
+  <img src=${values.image} class="toy-avatar" />
+  <p>${values.likes}</p>
+  <button class="like-btn" id=${values.id}>Like ❤️</button>
+    </div>`
+      document.getElementById('card').innerHTML=data1
+  })
+  
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
